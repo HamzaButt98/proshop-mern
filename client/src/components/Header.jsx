36b241e3fbe6a7b927fd39b7f0e42ea3f84d1b1b
unknown <m.hamza2456@gmail.com>
2023-12-09 23:12:16 +0500

    server and api stuff

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
-import logo from '../assets/logo.png';
+import logo from '/logo.png';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
